dd6f389f55c72cd56b68a36f2541db502b2d1ec5
Bakhtiyar Suleimenov <Bakhtiyar@MacBook.local>
2021-06-17 10:59:43 +0600

    refactor: JMSservice

--- newsapp-services/src/main/java/jms/JmsService.java
+++ newsapp-services/src/main/java/jms/JmsService.java
@@ -2,32 +2,35 @@ package jms;
 
 import entity.News;
 import org.slf4j.Logger;
-
-import javax.annotation.Resource;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.jms.*;
+import javax.naming.InitialContext;
 
 @ApplicationScoped
 public class JmsService {
-
-    @Resource(mappedName = "java:/jms/queue/NewsAppQueue")
-    private Queue newsAppQueue;
-
-    @Inject
-    @JMSConnectionFactory("java:/ConnectionFactory")
-    private JMSContext context;
 
     @Inject
     private Logger logger;
 
     public void sendMessage(News news) {
         try {
-            ObjectMessage objectMessage = context.createObjectMessage(news);
-            context.createProducer().send(newsAppQueue, objectMessage);
+            InitialContext context = new InitialContext();
+            QueueConnectionFactory factory =(QueueConnectionFactory)context.lookup("ConnectionFactory");
+            QueueConnection connection = factory.createQueueConnection();
+            connection.start();
+            QueueSession session = connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue queue = (Queue) context.lookup("java:/jms/queue/NewsAppQueue");
+
+            ObjectMessage objectMessage = session.createObjectMessage(news);
+            objectMessage.setJMSExpiration(60 * 60 * 1000);
+            objectMessage.setJMSPriority(9);
+
+            session.createSender(queue).send(objectMessage);
+
+            connection.close();
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
-
     }
 }
